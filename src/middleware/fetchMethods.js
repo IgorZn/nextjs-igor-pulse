c57a220f57fb93262895c1e457045c81c8a6d188
igor.znamensky <igor.znamensky@gmail.com>
2024-10-18 10:01:52 +0300

    25

--- src/middleware/fetchMethods.js
+++ src/middleware/fetchMethods.js
@@ -1,11 +1,29 @@
 import "dotenv/config";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || null
+
 export const fetchProperties = async () => {
-    return await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/properties`)
+    if(!API_URL) return []
+
+    return await fetch(`${API_URL}/api/properties`)
         .then(data => {
             return data.json();
         })
         .catch(error => {
             console.log(error);
+            return []
         });
 };
+
+export const fetchProperty = async (id) => {
+    if(!API_URL) return {}
+
+    return await fetch(`${API_URL}/api/properties/${id}`)
+        .then(data => {
+            return data.json();
+        })
+        .catch(error => {
+            console.log(error);
+            return {}
+        });
+}
