ebe9af024ff84748ecb293309aba41241fd69437
igor.znamensky <igor.znamensky@gmail.com>
2024-11-13 09:55:47 +0300

    51

--- src/app/api/bookmarks/route.jsx
+++ src/app/api/bookmarks/route.jsx
@@ -3,24 +3,35 @@ import { getSessionUser } from '@/../utils/getSessionUser'
 import { NextResponse } from 'next/server'
 import { User } from '@/../models/User'
 
+export const dynamic = 'force-dynamic'
+
 export const POST = async req => {
 	await connectDB()
 	const { id } = await req.json()
+	let message
 
 	// Получаем сессию
 	const { userId, user } = await getSessionUser()
 
-	await User.find({ _id: userId }, 'bookmarks').then(data => {
-		const [bookmarks] = data
-		console.log('bookmarks', bookmarks?.bookmarks)
-	})
-
 	if (!userId || !user) {
-		return NextResponse.json(
-			{ message: 'User ID or User not found' },
-			{ status: 401 }
-		)
+		return NextResponse.json({ message: 'User ID or User not found' }, { status: 401 })
 	}
 
-	return NextResponse.json({ message: 'Success' }, { status: 200 })
+	const userBookmarks = await User.findById({ _id: userId }, 'bookmarks')
+
+	try {
+		if (userBookmarks.bookmarks.includes(id)) {
+			userBookmarks.bookmarks.pull(id)
+			userBookmarks.save()
+			message = 'Bookmark removed'
+		} else {
+			userBookmarks.bookmarks.push(id)
+			userBookmarks.save()
+			message = 'Bookmark added'
+		}
+
+		return NextResponse.json({ message }, { status: 200 })
+	} catch (e) {
+		return NextResponse.json({ message: e.message }, { status: 500 })
+	}
 }
